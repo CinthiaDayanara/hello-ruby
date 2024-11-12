6438fe9525e106a5a3c757d203af402eb1d855d2
DayanaraChuraco <cdchuraco@uce.edu.ec>
2024-11-12 09:49:21 -0500

    up3

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
+# Gemfile
 source 'https://rubygems.org'
 
-gem 'rails', '6.1.4'
-gem 'puma', '~> 5.0'
+gem 'sinatra'
--- hello_world.rb
+++ hello_world.rb
@@ -1,1 +1,12 @@
-puts 'Hello, World!'
+# hello_world.rb
+require 'sinatra'
+
+# Obtén el puerto de la variable de entorno o usa un puerto por defecto
+port = ENV['PORT'] || 3000  # Usamos 3000 si no está definido el puerto en Railway
+
+# Establece el puerto para la aplicación
+set :port, port
+
+get '/' do
+  'Hello, World!'
+end
